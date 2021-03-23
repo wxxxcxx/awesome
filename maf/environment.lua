702acabd9c9237fc418e6790980831569dcf6412
meetcw <meetcw@outlook.com>
2021-03-23 18:22:42 +0800

    .

--- maf/environment.lua
+++ maf/environment.lua
@@ -1,6 +1,6 @@
 local module={}
 module.terminal = os.getenv('TERMINAL') or 'alacritty'
-module.browser = 'google-chrome'
+module.browser = 'google-chrome-stable'
 module.filemanager = module.terminal .. ' -e ranger'
 module.gui_editor = 'code'
 module.editor = os.getenv('EDITOR') or 'vim'
